d8959247bd1c6a1745fb4ef4497919e6fa8260c4
jaithra <jjaithra79@gmail.com>
2025-02-03 23:17:55 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Product from './Product'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Everyone</h1>
+      
+      <Product name = "Iphone 14" price={130000}/>
+      <Product name = "Samsung A12" price={20000}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Product.jsx
@@ -0,0 +1,11 @@
+function Product(props)
+{
+    return(
+        <>
+
+        <h1>Product Name : {props.name} and price is {props.price}</h1>
+        </>
+    )
+}
+
+export default Product
